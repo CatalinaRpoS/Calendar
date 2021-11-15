25e0db1608d628ac37bf139027af19022f38f7f4
CatalinaRpoS <rposalgadoc@gmail.com>
2021-11-14 22:12:45 -0500

    Now, works for sunday too

--- calendar.js
+++ calendar.js
@@ -6,7 +6,7 @@ function findDate() {
     
     if (year >= 1900 && year <= 2100) {
 
-        let u_year, s_year, d_year, month, d_month, s_month, day, d_date, date;
+        let u_year, s_year, d_year, month, d_month, s_month, day, d_date, date, bis;
 
         if (year < 2000) {
 
@@ -76,62 +76,56 @@ function findDate() {
 
         day = parseInt(document.getElementById("day").value);
 
-        if (s_month) {
+        if (s_year % 4 == 0){
 
-            if ((s_month == "Enero" || s_month == "Marzo" || s_month == "Mayo" || s_month == "Julio" || s_month == "Agosto" || s_month == "Octubre" || s_month == "Diciembre") && (day < 1 || day > 31)) {
-                document.getElementById("date").innerHTML = "Esta fecha no existe";
+            bis = "Bisiesto"
 
-            } else if ((s_month == "Abril" || s_month == "Junio" || s_month == "Septiembre" || s_month == "Noviembre") && (day < 1 || day > 30)) {
-                document.getElementById("date").innerHTML = "Esta fecha no existe";
-
-            } else if (s_month == "Febrero" && (day < 1 || day > 29)) {
-                document.getElementById("date").innerHTML = "Esta fecha no existe";
+            if (s_year % 100 == 0) {
                 
-            } else if (s_month == "Febrero" && day == 29) {
-
-                if ((s_year % 100 == 0 && s_year % 400 != 0) || (s_year % 4 != 0)) {
-                    document.getElementById("date").innerHTML = "Este año no es bisiesto";
-                }    
-                         
-            } else if ((s_year % 4 == 0 && month == "Enero") || (s_year % 4 == 0 && month == "Febrero" && day <= 29)) {
-                d_date = (d_date + 6) % 7;  
-
-            } else{
-                d_date = (d_year + d_month + day) % 7;
+                if (s_year % 400 == 0) {
+                    bis = "Bisiesto";
+                } else if (s_year % 400 != 0) {
+                    bis = "No bisiesto";
+                }
+                
             }
         }
 
-        if (d_date) {
-            switch (d_date) {
-                case 0:
-                    date = "Domingo";
-                    break;
-                case 1: 
-                    date = "Lunes";
-                    break;
-                case 2:
-                    date = "Martes";
-                    break;
-                case 3:
-                    date = "Miércoles";
-                    break;
-                case 4:
-                    date = "Jueves";
-                    break;
-                case 5:
-                    date = "Viernes";
-                    break;
-                case 6:
-                    date = "Sábado";
-                    break;                        
-                default:
-                    document.getElementById("date").innerHTML = "Esta fecha no existe";
-                    break;
-            }
-    
-            document.getElementById("date").innerHTML = date;
-        } 
-        
+        if (bis == "Bisiesto" && (s_month == "Enero" || s_month == "Febrero")) {
+            d_date = (d_year + d_month + day) % 7;
+            d_date = (d_date + 6) % 7;
+        } else{
+            d_date = (d_year + d_month + day) % 7;
+        }
+
+        switch (d_date) {
+            case 0:
+                date = "Domingo";
+                break;
+            case 1: 
+                date = "Lunes";
+                break;
+            case 2:
+                date = "Martes";
+                break;
+            case 3:
+                date = "Miércoles";
+                break;
+            case 4:
+                date = "Jueves";
+                break;
+            case 5:
+                date = "Viernes";
+                break;
+            case 6:
+                date = "Sábado";
+                break;                        
+            default:
+                document.getElementById("date").innerHTML = "Esta fecha no existe";
+                break;
+        }
+
+        document.getElementById("date").innerHTML = date;
     
     } else{
         document.getElementById("date").innerHTML = "El año que elegiste está fuera del rango";
